6f1cb7a0a7ca2f8c617aa134fd8e62ae5d030955
Kachi <kachdekan@gmail.com>
2023-09-12 14:38:21 +0300

    setup and try refresh tokens

--- packages/outre-auth/src/utils/generateToken.js
+++ packages/outre-auth/src/utils/generateToken.js
@@ -1,16 +1,50 @@
 const jwt = require('jsonwebtoken');
+const { db } = require('../fbconfig');
 
-const generateToken = (res, userId) => {
+const generateTokens = (res, userId, userPassword) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
+    expiresIn: "1m",
+  });
+
+  const refreshToken = jwt.sign({ userId }, process.env.JWT_SECRET_TWO + userPassword, {
+    expiresIn: '7d',
   });
 
   res.cookie('x-token', token, {
     httpOnly: true,
     secure: false, // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    maxAge:   60 * 1000, // 1 minute
+  });
+  
+  res.cookie('x-refresh-token', refreshToken, {
+    httpOnly: true,
+    secure: false, // Use secure cookies in production
+    sameSite: 'strict', // Prevent CSRF attacks
+    maxAge:  7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
 
-module.exports = { generateToken };
+const refreshTokens = async (res, refreshToken) => {
+  const decoded = jwt.decode(refreshToken);
+  if(decoded.userId){
+    const user = await db.collection('users').doc(decoded.userId).get();
+    const userPassword = user.data().password;
+    const verified = jwt.verify(refreshToken, process.env.JWT_SECRET_TWO + userPassword);
+    const userId = verified.userId;
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "1m",
+    });
+    res.cookie('x-token', token, {
+      httpOnly: true,
+      secure: false, // Use secure cookies in production
+      sameSite: 'strict', // Prevent CSRF attacks
+      maxAge:   60 * 1000, // 1 minute
+    });
+    return {userId};  
+  }else {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+}
+module.exports = { generateTokens, refreshTokens};
