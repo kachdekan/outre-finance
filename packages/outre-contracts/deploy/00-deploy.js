345509e1e1fc86c6b4f8ce191b228eea5e8d1a84
Kachi <kachdekan@gmail.com>
2023-10-06 21:21:45 +0300

    new depoly ment

--- packages/outre-contracts/deploy/00-deploy.js
+++ packages/outre-contracts/deploy/00-deploy.js
@@ -2,12 +2,12 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
-  /*
+  
   await deploy('Loans', {
     from: deployer,
     log: true,
-  })*/
-  
+  })
+  /*
   await deploy('LoanONRs', {
     from: deployer,
     log: true,
--- packages/xdc-outre-dapp/config/appconfig.js
+++ packages/xdc-outre-dapp/config/appconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   blockscoutApiURL: 'https://apothem.blocksscan.io/api/',
   stableToken: '0x1e2913E1aC339a4996353f8F58BE0de3D109b5A5', //USxD
   nativeToken: '0x03c0d9bc556BE68870B96976e81D32ebb49d335D', //XDC //Not the actual address
-  loansAddr: '0x95570D02c68DACda898F3E6E67A8490B06d7D08e',
-  ONRAddr: '0x11EC7Bf60143370e65AA8ddE9EA7C0c0D3b82A1e',
+  loansAddr: '0x527245Cc3421D7e9C8DA041897E9ea4E869273ec',
+  ONRAddr: '0x95A49CDA1aC3EA41Cb17080bbf3Ccf1d891a0459',
   chainData: {
     name: 'apothem',
     chainId: 51,
