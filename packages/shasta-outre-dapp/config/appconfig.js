e34e50e1082e825da31ce3a16ca34c231cfc312e
Kachi <kachdekan@gmail.com>
2023-11-28 21:50:05 +0000

    update mainnet addressed

--- packages/shasta-outre-dapp/config/appconfig.js
+++ packages/shasta-outre-dapp/config/appconfig.js
@@ -2,7 +2,7 @@ module.exports = {
   authURL: 'https://us-central1-clixpesa-test.cloudfunctions.net/outreauth',
   shastaApiURL: 'https://api.shasta.trongrid.io/v1',
   stableToken: 'TVMZYcbjpjaGqmciUjzuRWzANTXfHxrRCs',
-  loansAddr: 'THZ6EmzdGZEPU3P6g3oC8QHLtLbPwqG2xB',
-  ONRAddr: 'TE3VSogCBmUcjNqT7Xz45wbtusA7XkdbdN',
-  spacesAddr: 'TCiMc7XYDU6UNcE178SjYmXc7899iTCgDj',
+  loansAddr: 'TXEtA93dfhsMqgJVxwL9T654NSnr9AdS8q', //'THZ6EmzdGZEPU3P6g3oC8QHLtLbPwqG2xB',
+  ONRAddr: 'TXCxmLHNWaEwiAicGSCsowutsKH9ZgDx83', //'TE3VSogCBmUcjNqT7Xz45wbtusA7XkdbdN',
+  spacesAddr: 'TLoMqvkbrzogRSBzXiCTa9Ub7bdizuv6Hm', //'TCiMc7XYDU6UNcE178SjYmXc7899iTCgDj',
 };
